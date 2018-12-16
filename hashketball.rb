9bea0249a39fa3501b82d7c073a2ed33b3a5f773
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-16 04:31:46 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -17,50 +17,50 @@ def game_hash
   }
   home_name = ["Alan Anderson",	"Reggie Evans",	"Brook Lopez",	"Mason Plumlee",	"Jason Terry"]
   home_info = [ 
-    ['0', '16', '22', '12', '12', '3', '1', '1'], 
-    ['30', '14', '12', '12', '12', '12', '12', '7'], 
-    ['11', '17', '17', '19', '10', '3', '1', '15'], 
-    ['1', '19', '26', '12', '6', '3', '8', '5'], 
-    ['31', '15', '19', '2', '2', '4', '11', '1'] 
+    [0, 16, 22, 12, 12, 3, 1, 1], 
+    [30, 14, 12, 12, 12, 12, 12, 7], 
+    [11, 17, 17, 19, 10, 3, 1, 15], 
+    [1, 19, 26, 12, 6, 3, 8, 5], 
+    [31, 15, 19, 2, 2, 4, 11, 1] 
   ]
   game[:home][:team_name] = "Brooklyn Nets"
   game[:home][:colors] = ["Black", "White"]
   home_name.zip(home_info).each do |name, info|
     game[:home][:players].merge!({
       name => {
-        :number => info[0].to_i,
-        :shoe =>info[1].to_i,
-        :points => info[2].to_i,
-        :rebounds => info[3].to_i,
-        :assists => info[4].to_i,
-        :steals => info[5].to_i,
-        :blocks => info[6].to_i,
-        :slam_dunks => info[7].to_i
+        :number => info[0],
+        :shoe =>info[1],
+        :points => info[2],
+        :rebounds => info[3],
+        :assists => info[4],
+        :steals => info[5],
+        :blocks => info[6],
+        :slam_dunks => info[7]
       }
     })    
     end
     
     away_name = ["Jeff Adrien", "Bismak Biyombo", "DeSagna Diop", "Ben Gordon", "Brendan Haywood"]
     away_info = [
-      ['4', '18', '10', '1', '1', '2', '7', '2'],
-      ['0', '16', '12', '4', '7', '7', '15', '10'],
-      ['2', '14', '24', '12', '12', '4', '5', '5'],
-      ['8', '15', '33', '3', '2', '1', '1', '0'],
-      ['33', '15', '6', '12', '12', '22', '5', '12']
+      [4, 18, 10, 1, 1, 2, 7, 2],
+      [0, 16, 12, 4, 7, 7, 15, 10],
+      [2, 14, 24, 12, 12, 4, 5, 5],
+      [8, 15, 33, 3, 2, 1, 1, 0],
+      [33, 15, 6, 12, 12, 22, 5, 12]
     ]
     game[:away][:team_name] = "Charlotte Hornets"
     game[:away][:colors] = ["Turquoise", "Purple"]
     away_name.zip(away_info).each do |name, info|
     game[:away][:players].merge!({
       name => {
-        :number => info[0].to_i,
-        :shoe =>info[1].to_i,
-        :points => info[2].to_i,
-        :rebounds => info[3].to_i,
-        :assists => info[4].to_i,
-        :steals => info[5].to_i,
-        :blocks => info[6].to_i,
-        :slam_dunks => info[7].to_i
+        :number => info[0],
+        :shoe =>info[1],
+        :points => info[2],
+        :rebounds => info[3],
+        :assists => info[4],
+        :steals => info[5],
+        :blocks => info[6],
+        :slam_dunks => info[7]
       }
     })    
     end
